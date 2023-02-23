ba623a87d21d9897be908e62efa60cdd1ce6a3a8
blastz <touchzh1996@outlook.com>
2023-02-23 16:15:32 +0800

    build(next): add app name

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import styles from "./page.module.css";
+
+const AppInfo = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    fetch("/api/app-info")
+      .then((o) => o.json())
+      .then((o) => setName(o.name));
+  }, []);
+
+  return <p>{name}</p>;
+};
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>Nextjs Docker Template</p>
+        <AppInfo />
       </div>
     </main>
   );
